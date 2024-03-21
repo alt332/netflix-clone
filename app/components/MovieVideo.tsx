cc738b34988af0de1d5862f3d8526d8607f1675d
Thiha Tun <alt.332@gmail.com>
2024-03-21 08:29:36 +0630

    Minor changes.

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,6 +1,7 @@
 export default function MovieVideo() {
   return (
     <div>
+      <h3>Hooray!!</h3>
       <p>Welcome to our movie video component!</p>
       <p>Enjoy watching your favorite movies here.</p>
     </div>
